76c40ac6d0f2df6f577da87c1638a613b703edf1
Arin Ved Sinha <arinvedsinha22@gmail.com>
2025-06-26 22:43:53 +0530

    Notification Fix 4

--- app/root.tsx
+++ app/root.tsx
@@ -115,7 +115,7 @@ function LoadingScreen() {
 }
 
 // Import feedback components
-import { SocialMediaIcons } from '../app/components/SocialMediaIcons';
+import { SocialMediaIcons } from '~/components/SocialMediaIcons.tsx';
 import { DynamicFeedback } from './components/feedback/DynamicFeedback';
 
 export const links: LinksFunction = () => [
